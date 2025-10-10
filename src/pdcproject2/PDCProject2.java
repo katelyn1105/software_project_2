fea48e7336bac7168f5dd3a0a7c24f3aa1796d26
Katelyn Correia <katelyncorreia@Katelyns-Laptop.local>
2025-10-10 17:11:03 +1300

    Initial commit

---
+++ src/pdcproject2/PDCProject2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdcproject2;
+import java.awt.*;
+import javax.swing.*;
+/**
+ *
+ * @author 1708k
+ */
+public class PDCProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       Window window = new Window();
+       JFrame frame = new JFrame();
+        
+        
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setTitle("K&K's Ergonomic Adventure!");
+       frame.setResizable(true);
+       frame.setLocationRelativeTo(null);
+       frame.setVisible(true);
+        
+       
+       
+       frame.add(window);
+       frame.pack();
+        
+    }
+    
+}
---
+++ src/pdcproject2/Window.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdcproject2;
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author 1708k
+ */
+public class Window extends JPanel{
+    
+    
+    
+    
+    
+    //Placeholder until we palaver about how we want to do this. - Assuming your resolution isn't < 480
+    final int screenWidth = 480;
+    final int screenHeight = 480;
+    
+    
+    public Window(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.DARK_GRAY);
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
